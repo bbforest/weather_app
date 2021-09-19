244b88e746742e7a7b30199a852e94eb0b157ec1
에케EKE <eke@bbforest.net>
2021-09-20 02:37:13 +0900

    fix: Types of weather
    I added type of Korean weather.
    한국어 날씨 종류를 추가했습니다.

--- Weather.js
+++ Weather.js
@@ -4,16 +4,31 @@ import PropTypes from "prop-types";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function Weather({ temp, cond, temp_min, temp_max, feels_like, icon }) {
+    //한국어가 존재하면 표시, 없으면 영어로 표시
+    var local_cond, local_icon;
+    if(Korean[cond]?.name == null) {
+        local_cond = cond;
+        local_icon = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+    }
+    else{
+        local_cond = Korean[cond]?.name;
+        local_icon = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+    }
+
     return (
         <LinearGradient colors={["#68B1FF", "#82BEFF"]} style={style.container}>
             <StatusBar barStyle="light-content" />
             <View style={style.container}>
-                <Image style={style.logo} source={{uri: `http://openweathermap.org/img/wn/${icon}@4x.png`}} />
+                {/* 아이콘 */}
+                <Image style={style.logo} source={{uri: local_icon}} />
+                {/* 19°C */}
                 <Text style={{ fontSize: 50 }}>{temp}°C</Text>
+                {/* 17° / 21° | 체감온도 18° */}
                 <Text style={{ fontSize: 20 }}><Text style={{ color: "blue" }}>{temp_min}°</Text> / <Text style={{ color: "red" }}>{temp_max}°</Text> | 체감온도 {feels_like}°</Text>
             </View>
             <View style={style.container}>
-                <Text style={{fontSize: 50}}>{Korean[cond]}</Text>
+                {/* 맑음 */}
+                <Text style={{fontSize: 50}}>{local_cond}</Text>
             </View>
         </LinearGradient>
     );
@@ -24,7 +39,27 @@ Weather.PropTypes = {
 };
 
 const Korean = {
-    Clear: "맑음"
+    Clear: {
+        name: "맑음"
+    },
+    Thunderstorm: {
+        name: "뇌우"
+    },
+    Drizzle: {
+        name: "이슬비"
+    },
+    Rain: {
+        name: "비"
+    },
+    Snow: {
+        name: "눈"
+    },
+    Clouds: {
+        name: "구름"
+    },
+    Mist: {
+        name: "안개"
+    }
 };
 
 const style = StyleSheet.create({
